a0e6f53b2a54a85bfa5fb1d4a6c888b0ac443c9a
borecjeborec1 <robingrundel@seznam.cz>
2023-02-04 17:26:20 +0100

    rozsvecovani svetel

--- Script/Svíčka.gd
+++ Script/Svíčka.gd
@@ -2,20 +2,29 @@ extends Area2D
 
 var jde_doprava = false
 
-
-func _ready():
-			$AnimatedSprite.play("Idle")
-
+var je_zapnuta = false
 
 
 func _process(delta):
-	if jde_doprava:
-		if $"Světlo".position.x > 1.5:
-			jde_doprava = false
-		$"Světlo".position.x += .3
-	else:
-		if $"Světlo".position.x < .3:
-			jde_doprava = true
-		$"Světlo".position.x -= .3
+	if je_zapnuta:
+		$AnimatedSprite.play("Idle")
+		if jde_doprava:
+			if $"Světlo".position.x > 1.5:
+				jde_doprava = false
+			$"Světlo".position.x += .3
+		else:
+			if $"Světlo".position.x < .3:
+				jde_doprava = true
+			$"Světlo".position.x -= .3
 
+
+
+
+func _on_Svka_body_entered(body):
+	if body.name == "Player":
+		if body.sebrane_zapalky > 0:
+			$"Světlo".visible = true
+			$Particles2D.visible = true
+			je_zapnuta = true
+			body.sebrane_zapalky-=1
 
--- Script/Zápalka.gd
+++ Script/Zápalka.gd
@@ -4,5 +4,3 @@ func _on_Zpalka_body_entered(body):
 	if body.name == "Player":
 		body.sebrane_zapalky += 1
 		queue_free()
-	print(body.sebrane_zapalky)
-	pass # Replace with function body.
